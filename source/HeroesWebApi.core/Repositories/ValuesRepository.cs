e638deb8632357ee72181b7b12b6a9b42d4dae4c
kojurama <kojurama@frontier.com>
2019-08-28 03:00:02 -0700

    tour-backend
    c# backend for the tour-of-heroes angular tutorial

---
+++ source/HeroesWebApi.core/Repositories/ValuesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesWebApi.core.Repositories
+{
+    public interface IValuesRepository
+    {
+        string[] GetValues();
+    }
+
+    public class ValuesRepository : IValuesRepository
+    {
+        public string[] GetValues() => new string[] { "value1", "value2" };
+    }
+}
---
+++ source/HeroesWebApi.core/Services/ValueServices.cs
@@ -0,0 +1,23 @@
+﻿using HeroesWebApi.core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HeroesWebApi.core.Services
+{
+    public interface IValuesService
+    {
+        string[] GetValues();
+    }
+    public class ValuesService : IValuesService
+    {
+        private readonly IValuesRepository valuesRepository;
+
+        public ValuesService(IValuesRepository valuesRepository)
+        {
+            this.valuesRepository = valuesRepository;
+        }
+
+        public string[] GetValues() => valuesRepository.GetValues();
+    }
+}
